ceef371bccf2629e985aebd3852634cd44b70e2a
Patricio Otel <patricio87209@gmail.com>
2020-04-02 16:14:03 -0300

    initial commit

---
+++ src/components/Box.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import '../../src/App.css'
+
+function Box(props){
+  return (
+    <div className='Box'>
+      <h1> Box </h1>
+      </div>
+  );
+}
+
+export default Box;
---
+++ src/components/Circle.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../../src/App.css'
+class Circle extends Component {
+
+  render() {
+    return (
+      <div className='Circle'>
+        <h1> Circle </h1>
+        </div>
+    );
+  }
+}
+
+export default Circle;
