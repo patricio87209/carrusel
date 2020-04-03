f48b9880c7a7ccf71a529b9dc4e1888ef12f9540
Patricio Otel <patricio87209@gmail.com>
2020-04-03 08:15:30 -0300

    Updates

---
+++ precache-manifest.727ae580243f642724352d7139014fc8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f98a14e006f54bae06656f904eda724b",
+    "url": "/carrusel/index.html"
+  },
+  {
+    "revision": "c1b203e832069ef77bfc",
+    "url": "/carrusel/static/css/main.0c54e0e9.chunk.css"
+  },
+  {
+    "revision": "f655e912b97d8434a266",
+    "url": "/carrusel/static/js/2.3927c2bb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/carrusel/static/js/2.3927c2bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1b203e832069ef77bfc",
+    "url": "/carrusel/static/js/main.12abfb6b.chunk.js"
+  },
+  {
+    "revision": "d022abb13887c0ec4a0f",
+    "url": "/carrusel/static/js/runtime-main.c3170583.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/carrusel/static/media/logo.5d5d9eef.svg"
+  }
+]);
